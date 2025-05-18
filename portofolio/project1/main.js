8df630c3b3211d7ba21f07f75ca7a90c9c8c7fc1
Ryan <umarryan04@gmail.com>
2025-05-19 01:22:07 +0800

    Add files via upload

---
+++ portofolio/project1/main.js
@@ -0,0 +1,19 @@
+let nama = "ryan";
+let umur = 20;
+let alamt = "surabaya";
+
+function generatedBiodata () {
+    if (umur > 2 && umur < 7) {
+        console.log(`anda anak kecil`);
+    } else if (umur > 7 && umur < 12) { console.log(`anda anak anak`);
+
+    } else{
+        (umur > 20 && umur< 10)
+    } alert(`anda bau tanah`);
+}
+
+generatedBiodata (){
+    console.log(`nama saya ${nama}`);
+    console.log(`umur saya ${umur}`);
+    console.log(`alamat saya ${alamt}`);
+}
